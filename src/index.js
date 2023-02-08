e304785be8f343d9645e9a2ab115d120c4fe45a0
nnazgul <nazgul.nurmatova.ch@gmail.com>
2023-02-08 14:06:30 +0600

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let stuck = [];
+    const OPEN_BRACKETS = ['(', '{', '[', '1', '3', '5',];
+    const BRACKETS_PAIR = {
+        [')']: '(',
+        ['}']: '{',
+        [']']: '[',
+        ['2']: '1',
+        ['4']: '3',
+        ['6']: '5',
+    };
+    const POLINDROM_BRACKTS = ['|', '7', '8'];
+
+    for (let i = 0; i < str.length; i++) {
+        let currentSymbol = str[i];
+        let topElement = stuck[stuck.length - 1];
+        if (bracketsConfig.includes(currentSymbol) || OPEN_BRACKETS.includes(currentSymbol) || POLINDROM_BRACKTS.includes(currentSymbol)) {
+            if (currentSymbol === topElement && POLINDROM_BRACKTS.includes(currentSymbol)) {
+                stuck.pop();
+            } else {
+                stuck.push(currentSymbol);
+            }
+
+        } else {
+            if (stuck.length === 0) {
+                return false;
+            }
+
+            if (BRACKETS_PAIR[currentSymbol] === topElement) {
+                stuck.pop();
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+    return stuck.length === 0;
 }
